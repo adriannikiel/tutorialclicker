cb910885f5469ebeacedef1445d0b666878cee8f
adriannikiel <adrian.nikiel@gmail.com>
2017-03-19 16:05:59 +0100

    added AbstractScreen and SplashScreen

---
+++ core/src/pl/javadevmatt/tutorialclicker/screens/SplashScreen.java
@@ -0,0 +1,31 @@
+package pl.javadevmatt.tutorialclicker.screens;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import pl.javadevmatt.tutorialclicker.TutorialClickerGame;
+
+public class SplashScreen extends AbstractScreen {
+	
+	private Texture splashImg;
+
+	public SplashScreen(TutorialClickerGame game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+		// TODO implement better assets loading when game grows
+		splashImg = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		
+		spriteBatch.begin();
+		spriteBatch.draw(splashImg, 0, 0);
+		spriteBatch.end();
+	}
+	
+	
+}

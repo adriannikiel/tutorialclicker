946bd7dc1fa18108cbaeda0ad481d59085076ede
adriannikiel <adrian.nikiel@gmail.com>
2017-06-27 21:03:32 +0200

    added player button and click listener

--- core/src/pl/javadevmatt/tutorialclicker/screens/GameplayScreen.java
+++ core/src/pl/javadevmatt/tutorialclicker/screens/GameplayScreen.java
@@ -1,11 +1,17 @@
 package pl.javadevmatt.tutorialclicker.screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 import pl.javadevmatt.tutorialclicker.TutorialClickerGame;
 import pl.javadevmatt.tutorialclicker.entities.Player;
 
 public class GameplayScreen extends AbstractScreen{
 	
 	private Player player;
+	private Button playerButton;
 
 	public GameplayScreen(TutorialClickerGame game) {
 		super(game);
@@ -14,11 +20,37 @@ public class GameplayScreen extends AbstractScreen{
 	@Override
 	protected void init() {
 		initPlayer();
+		initPlayerButton();
 	}
 
 	private void initPlayer() {
 		player = new Player();
 		stage.addActor(player);
+	}
+	
+	private void initPlayerButton() {
+		playerButton = new Button(new ButtonStyle());
+		playerButton.setWidth(460);
+		playerButton.setHeight(360);
+		playerButton.setX(10);
+		playerButton.setY(170);
+		playerButton.setDebug(true);
+		
+		stage.addActor(playerButton);
+		
+		playerButton.addListener(
+				new ClickListener() {
+				
+					@Override
+					public boolean touchDown(InputEvent event, float x,
+							float y, int pointer, int button) {
+						System.out.println("click");
+						return super.touchDown(event, x, y, pointer, button);
+					}
+					
+				});
+		
+		
 	}
 	
 	@Override

718d1fd6cb452a67d0f81e8dbdc16471018f7f6f
adriannikiel <adrian.nikiel@gmail.com>
2017-06-30 23:25:51 +0200

    saving player score

--- core/src/pl/javadevmatt/tutorialclicker/TutorialClickerGame.java
+++ core/src/pl/javadevmatt/tutorialclicker/TutorialClickerGame.java
@@ -4,11 +4,15 @@ import pl.javadevmatt.tutorialclicker.screens.SplashScreen;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TutorialClickerGame extends Game {
+	
+	public final static String GAME_PREFS = "pl.javadevmatt.preclicker.prefs";
+	public final static String GAME_SCORE = "pl.javadevmatt.preclicker.prefs.score";
 	
 	public final static String GAME_NAME = "Tutorial Clicker";
 	
@@ -17,15 +21,29 @@ public class TutorialClickerGame extends Game {
 	
 	private boolean paused;
 	
+	private Preferences prefs;
+	
 	private int points;
 
 	@Override
 	public void create () {
+		init();
 		this.setScreen(new SplashScreen(this));
+	}
+
+	private void init() {
+		prefs = Gdx.app.getPreferences(GAME_PREFS);
+		loadScore();
+	}
+
+	private void loadScore() {
+		points = prefs.getInteger(GAME_SCORE);
 	}
 
 	public void addPoint() {
 		points++;
+		prefs.putInteger(GAME_SCORE, points);
+		prefs.flush();
 	}
 	
 	/**
